0c5c4283886cf1c6a3856b0095521bb4f981b9a9
Jon <jon@pidev.local.jondurrant.com>
2021-08-25 10:31:49 +0100

    wi

---
+++ bin/picoDeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Using openocd to deply ${1}
+cd ${HOME}/src/openocd/tcl
+openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program ${1} verify reset exit"
+echo Deployed
+arm-none-eabi-size ${1}
+
