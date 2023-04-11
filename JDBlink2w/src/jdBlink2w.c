c0107718a0a28f150804aae96dfce562633ec495
Jon Durrant <jon@jondurrant.com>
2023-04-11 09:56:49 +0100

    Added in PicoW Blink Examples

---
+++ JDBlink2w/src/jdBlink2w.c
@@ -0,0 +1,27 @@
+/**
+ * Jon Durrant.
+ *
+ * Blink LED in patter of twos
+ */
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+#define DELAY_SHORT 200 // in microseconds
+#define DELAY_LONG 500 // in microseconds
+
+int main() {
+	cyw43_arch_init();
+
+    while (true) { // Loop forever
+    	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+		sleep_ms(DELAY_SHORT);
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+		sleep_ms(DELAY_SHORT);
+    	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+		sleep_ms(DELAY_SHORT);
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+		sleep_ms(DELAY_LONG);
+    }
+
+}
