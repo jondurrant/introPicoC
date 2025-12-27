7e8ac342fc400eb4c60a81d8e28914ccea3554eb
Jon Durrant <jon@jondurrant.com>
2025-12-27 07:03:28 +0000

    Pushed VSCode settings to use SDK 2.2.0

--- bin/picoDeploy.bat
+++ bin/picoDeploy.bat
@@ -3,4 +3,4 @@
 set elf=%1
 set interface=cmsis-dap.cfg 
 
-openocd -s "c:\Program Files\Raspberry Pi\Pico SDK v1.5.0\openocd\scripts" -f  interface\%interface%  -f target\rp2040.cfg -c "adapter speed 5000" -c "program %elf% verify reset exit"
+openocd -s "c:\Program Files\Raspberry Pi\Pico SDK v2.2.0\openocd\scripts" -f  interface\%interface%  -f target\rp2040.cfg -c "adapter speed 5000" -c "program %elf% verify reset exit"
